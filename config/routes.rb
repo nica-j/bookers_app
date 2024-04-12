fa522c27e3fd136e65fb3af1f827bb647a6d4bab
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-04-12 13:22:08 +0000

    finish top-text

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  
+  # GET：リソースの取得
+  # POST：リソースの更新、リソースへのデータ追加 など
+  # DELETE：リソースの削除
+
 end
